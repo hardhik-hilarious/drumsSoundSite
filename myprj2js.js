fb90a946fc071f31c446ff0eaf201dff5538ad31
hardhik-hilarious <97403982+hardhik-hilarious@users.noreply.github.com>
2023-11-01 21:53:36 +0530

    Add files via upload

---
+++ myprj2js.js
@@ -0,0 +1,43 @@
+
+document.getElementById("b1").addEventListener("click",nokku1);
+document.getElementById("b2").addEventListener("click",nokku2);
+document.getElementById("b3").addEventListener("click",nokku3);
+document.getElementById("b4").addEventListener("click",nokku4);
+document.getElementById("b5").addEventListener("click",nokku5);
+document.getElementById("b6").addEventListener("click",nokku6);
+document.getElementById("b7").addEventListener("click",nokku7);
+function nokku(){
+    var audio=new Audio('sounds/crash.mp3');
+    audio.play();
+}
+function nokku1(){
+    var audio=new Audio('sounds/kick-bass.mp3');
+    audio.play();
+}
+function nokku2(){
+    var audio=new Audio('sounds/snare.mp3');
+    audio.play();
+}
+function nokku3(){
+    var audio=new Audio('sounds/tom-1.mp3');
+    audio.play();
+}
+function nokku4(){
+    var audio=new Audio('sounds/tom-2.mp3');
+    audio.play();
+}
+function nokku5(){
+    var audio=new Audio('sounds/tom-3.mp3');
+    audio.play();
+}
+function nokku6(){
+    var audio=new Audio('sounds/tom-4.mp3');
+    audio.play();
+}
+function nokku7(){
+    var audio=new Audio('sounds/crash.mp3');
+    audio.play();
+}
+
+
+
